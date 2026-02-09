35267aee8c29b892ab332ee24d5a7d09974e5f3d
Mykhailo Kobryn <mykhailo.kobryn+tait@eleks.com>
2026-02-10 00:14:20 +0200

    Completed task 3

--- homework_03.py
+++ homework_03.py
@@ -33,9 +33,35 @@ def get_numbers_ticket(min, max, quantity):
 
 # Task 3:
 
-def normalize_phone(phone_number)
-    pass
+import re
 
+def normalize_phone(phone_number):
+    cleaned_phone = re.sub(r'[^+\d]', '', phone_number)
+    if cleaned_phone.startswith("+"):
+        return cleaned_phone
+    elif cleaned_phone.startswith("380"):
+        return "+" + cleaned_phone
+    elif cleaned_phone.startswith("0"):
+        return "+38" + cleaned_phone
+    else:
+        return "+380" + cleaned_phone
     
+raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   ",
+]
+
+sanitized_numbers = [normalize_phone(num) for num in raw_numbers]
+    
+print(f"Normalized phone numbers for SMS: {sanitized_numbers}") # testing the output of the function that sanityzes the phone numbers
+
+
 
 
